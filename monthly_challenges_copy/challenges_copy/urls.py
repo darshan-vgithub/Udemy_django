e6441086150c3c8a5f2cbb6e3e50e16340691238
Darshan.V <darshanv487@gmail.com>
2025-06-26 11:35:48 +0530

    added the path segment so that it renders dynamically

--- monthly_challenges_copy/challenges_copy/urls.py
+++ monthly_challenges_copy/challenges_copy/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns=[
-path("january",views.january),
-path("february",views.february)
+# path("january",views.january),
+# path("february",views.february),
+path("<month>",views.monthly_challenge),
 ]
--- monthly_challenges_copy/challenges_copy/views.py
+++ monthly_challenges_copy/challenges_copy/views.py
@@ -7,4 +7,12 @@ def january(request):
 def february(request):
     return HttpResponse("February: Show love, not just to others but also to yourself. Self-care is important.")
 
+def monthly_challenge(request,month):
+    challenge_text=None
+    if month=="january":
+        challenge_text="Start fresh! Set clear goals and begin the year with positive energy."
+    elif month=="february":
+        challenge_text="Show love, not just to others but also to yourself. Self-care is important."
+    return HttpResponse(f"the month is {month} \n and the challenge is {challenge_text}")    
+
 # Create your views here.
