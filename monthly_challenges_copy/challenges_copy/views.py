df90048f5553dd3073f72e8e6fec206f6e6f59eb
Darshan.V <darshanv487@gmail.com>
2025-06-26 11:27:29 +0530

    added the views and set the urls

--- monthly_challenges_copy/challenges_copy/views.py
+++ monthly_challenges_copy/challenges_copy/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def january(request):
+    return HttpResponse("January: Start fresh! Set clear goals and begin the year with positive energy.")
+
+def february(request):
+    return HttpResponse("February: Show love, not just to others but also to yourself. Self-care is important.")
 
 # Create your views here.
